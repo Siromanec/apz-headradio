55bfd1ef0be875df7ffa1c798ec4ea9e5947bb6b
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-05-15 15:07:16 +0300

    basic styles

--- src/Profile.js
+++ src/Profile.js
@@ -3,6 +3,7 @@ import spotifyIcon from "./spotify_icon.svg";
 import ReactPlayer from "react-player/lazy";
 import ReactAudioPlayer from "react-audio-player";
 import song from "./staying-alive.mp3";
+import "./Profile.css"
 import PhotoChange from "./PhotoChange";
 import React, {useState} from "react";
 
@@ -17,23 +18,35 @@ export default function Profile() {
       fetch('https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5', {method: "POST", body: formData, mode: 'no-cors'}).then((response)=>response.json()).then((result)=>{console.log(result)})
   }
   const handleClick = () => {window.location.replace("https://open.spotify.com/track/6yLOGWTvvVXyPBEJUkaFZG?si=6666fde48d6947a1")}
-  const handlePhoto = () => {setShow(show => true)}
+  const handlePhoto = () => {setShow(show => !show)}
   return (
     <main>
-      <section>
+      <section className="profileInfo">
         <div className="profilePictureDiv" onClick={handlePhoto}>
           <img src={photo} className="profilePicture" />
           {show ? <PhotoChange onClick={submitHandler}/> : null}
-        </div>
         <div className="tag">@beheni</div>
         <div className="status">
           <span>Feeling stable</span>
         </div>
-      </section>
+        </div>
       <div className="headRadio" onClick={handleClick}>
-        <div>{songName}</div>
-        <img src={spotifyIcon} style={{ width: "50px" }}></img>
-      </div>
+        <div className="Song">
+          <img src={spotifyIcon} style={{ width: "50px" }}></img>
+          <div>{songName}</div>
+        </div>
+        <div className="Stats">
+          <div className="Posts">
+            <p>Posts</p>
+            <p>23</p>
+          </div>
+          <div className="Friends">
+            <p>Friends</p>
+            <p>10</p>
+          </div>
+          </div>
+        </div>
+      </section>
       <section className="recentDiary"></section>
     </main>
   );
