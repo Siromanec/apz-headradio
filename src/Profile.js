872bebd062a13f4484ff9afff35adef2178e7946
Siromanec <serhii.ivanov@ucu.edu.ua>
2023-05-15 14:33:02 +0300

    added text adder

--- src/Profile.js
+++ src/Profile.js
@@ -3,10 +3,52 @@ import spotifyIcon from "./spotify_icon.svg";
 import ReactPlayer from "react-player/lazy";
 import ReactAudioPlayer from "react-audio-player";
 import song from "./staying-alive.mp3";
-function UserIcon() {
-  return;
-}
+import { useState } from "react";
 
+// const loremIpsumHead = "Lorem ipsum"
+// // const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// const loremIpsum = "asdc"
+function Posts({ posts }) {
+  const listItems = posts.map((post) => (
+    <Post text={post.text} header={post.header}></Post>
+  ));
+  return <div>{listItems}</div>;
+}
+function Post({ text, header }) {
+  return (
+    <div>
+      <h3>{header}</h3>
+      <p>{text}</p>
+    </div>
+  );
+}
+function AddContent({ onClick }) {
+  return (
+    <button onClick={onClick} style={{ width: "50px", height: "50px" }}>
+      +
+    </button>
+  );
+}
+function Editor() {
+  const loremIpsumHead = "Lorem ipsum";
+  const loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  console.log(loremIpsumHead);
+  const [posts, setPosts] = useState(
+    Array({ text: loremIpsum, header: loremIpsumHead })
+  );
+  function handleClick() {
+    const nextPosts = posts.slice();
+    nextPosts.push({ text: loremIpsum, header: loremIpsumHead });
+    setPosts(nextPosts);
+  }
+  return (
+    <div>
+      <Posts posts={posts}></Posts>
+      <AddContent onClick={handleClick}></AddContent>
+    </div>
+  );
+}
 export default function Profile() {
   const songName = "Staying Alive";
   return (
@@ -23,9 +65,10 @@ export default function Profile() {
         <div>{songName}</div>
         <img src={spotifyIcon} style={{ width: "50px" }}></img>
         <ReactAudioPlayer src={song} controls />
-
       </section>
       <section className="recentDiary"></section>
+      <section className="textField"></section>
+      <Editor></Editor>
     </main>
   );
 }
