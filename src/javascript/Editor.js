cd4b17acb44eaf845d909bb2362b40961e7ecd99
OlesiaOmelchuk <olesyao2003@gmail.com>
2023-05-21 19:31:43 +0300

    made editor prettier

--- src/javascript/Editor.js
+++ src/javascript/Editor.js
@@ -1,17 +1,17 @@
 import { Editor } from "@tinymce/tinymce-react";
 import { useRef } from "react";
-import "../css/mycontent.css"
-import {tinymceAPIKey} from "./APIKeys"
+import "../css/Editor.css";
+import { tinymceAPIKey } from "./APIKeys";
 // import { Global } from "@emotion/core";
 export default function EditorWrapper() {
   const editorRef = useRef(null);
   const save = () => {
     // if (editorRef.current) {
-        console.log(editorRef)
-      const content = editorRef.current.getContent();
-      editorRef.current.setContent("");
-      // an application would save the editor content to the server here
-      console.log(content);
+    console.log(editorRef);
+    const content = editorRef.current.getContent();
+    editorRef.current.setContent("");
+    // an application would save the editor content to the server here
+    console.log(content);
     // }
   };
   return (
@@ -19,11 +19,16 @@ export default function EditorWrapper() {
       <Editor
         apiKey={tinymceAPIKey}
         initialValue=""
-        onInit={(evt, editor) => editorRef.current = editor}
+        onInit={(evt, editor) => (editorRef.current = editor)}
         init={{
-        content_css: "./mycontent.css",
+          menubar: false,
+          statusbar: false,
+          quickbars_insert_toolbar: false,
+          quickbars_selection_toolbar: false,
+          plugins: ["quickbars"],
+          toolbar: "blocks | bold italic strikethrough | quickimage",
+          content_css: "../css/Editor.css",
           selector: "#postEditor", // change this value according to your HTML
-          // menubar: 'edit',
           a_plugin_option: true,
           a_configuration_option: 400,
           hidden_input: true,
