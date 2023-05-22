3a9269e3bda0203779e119063dba8f3520ba2365
beheni <anastasiia.beheni@ucu.edu.ua>
2023-05-22 16:41:01 +0300

    added hyperlinks

--- src/javascript/Home.js
+++ src/javascript/Home.js
@@ -10,7 +10,6 @@ export default function Home() {
             date={"2 MAY 2023"}
             text={"Alonso is greatest driver of all time. Everyone who thinks different — is wrong. He won championships in bad cars and always showed great results. Alonso FTW"}
         ></PostBase>
-
         <FriendSongElement songName="Окситоцин  •  Ницо Потворно" avatar={Avatar}></FriendSongElement>
     </div>)
     }
