64e0cd2e90a0c776f686a5d7ba9378c4d5d911cb
Sofiia Folvarochna <sofiyafolv@gmail.com>
2023-05-23 18:10:28 +0300

    minor

--- src/javascript/ChangeSong.js
+++ src/javascript/ChangeSong.js
@@ -1,8 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "../css/ChangeSong.css";
+import crossButton from "../data/cross.svg"
 
+export default function ChangeSong() {
+    const [popUpClass, setPopUpClass] = useState("popup-change");
+    function handleClick() {
+        setPopUpClass("popup-change show")
+    }
 
-export default function ChangeSong(){
-    return <div className="lower-button">
-        <button >Change Song</button>
-        
+    function exitClick() {
+        setPopUpClass("popup-change")
+    }
+    return <div className="changeSong">
+        <div className={popUpClass}>
+            <div className="insert-song">
+                <span>Insert song link</span>
+                <input type="text" placeholder="..." />
+                <button className="exitButton" onClick={exitClick}>
+                    <img src={crossButton} />
+                </button>
+            </div>
+        </div>
+        <div className="lower-button">
+            <button onClick={handleClick}>Change Song</button>
+        </div>
     </div>
 }
