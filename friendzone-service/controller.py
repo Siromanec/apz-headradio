0e88a00d7cb01365b851d604d635f358854163fa
OlesiaOmelchuk <olesyao2003@gmail.com>
2024-05-11 16:44:43 +0300

    created friendzone interface

---
+++ friendzone-service/controller.py
@@ -0,0 +1,22 @@
+import fastapi
+from fastapi import FastAPI, Response, status
+
+import service
+
+app = FastAPI()
+
+@app.get("/get-friends/")
+async def get_friends(user: str, response: Response):
+    friends = service.get_friends(user)
+    response.status_code = status.HTTP_200_OK
+    return {"friends": friends}
+
+@app.post("/add-friend/")
+async def add_friend(user1: str, user2: str, response: Response):
+    service.add_friend(user1, user2)
+    response.status_code = status.HTTP_200_OK
+
+@app.post("/remove-friend/")
+async def remove_friend(user1: str, user2: str, response: Response):
+    service.remove_friend(user1, user2)
+    response.status_code = status.HTTP_200_OK
