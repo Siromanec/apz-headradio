652ff12703bffb63f6ee0548141ba4f1cf416e56
Siromanec <serhii.ivanov@ucu.edu.ua>
2024-05-11 16:33:06 +0300

    created likes interface

--- likes/controller.py
+++ likes/controller.py
@@ -1,8 +1,35 @@
 import fastapi
+import requests.status_codes
 from fastapi import FastAPI
-
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, Request, Response, status
 import service
 
 app = FastAPI()
 
-# get/post stuff, controller
+
+@app.get("/has-liked/")
+async def has_liked(user: str, post: int, response: Response):
+    liked = service.has_liked(user, post)
+    response.status_code = status.HTTP_200_OK
+    return {"has_liked": liked}
+
+
+@app.post("/add-like/")
+async def add_like(user: str, post: int, response: Response):
+    service.add_like(user, post)
+    response.status_code = status.HTTP_200_OK
+
+
+@app.post("/remove-like/")
+async def remove_like(user: str, post: int, response: Response):
+    service.remove_like(user, post)
+    response.status_code = status.HTTP_200_OK
+
+
+@app.get("/get-likes/")
+async def get_likes(post: int, response: Response):
+    likes = service.get_likes(post)
+    response.status_code = status.HTTP_200_OK
+    return {"likes": likes}
