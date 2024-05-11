7830dd79b30e15d6e5ce790a50dabef18ee3f357
Siromanec <serhii.ivanov@ucu.edu.ua>
2024-05-11 15:44:38 +0300

    added service structure

---
+++ likes/controller.py
@@ -0,0 +1,8 @@
+import fastapi
+from fastapi import FastAPI
+
+import service
+
+app = FastAPI()
+
+# get/post stuff, controller
---
+++ likes/main.py
@@ -0,0 +1,5 @@
+import uvicorn
+from controller import app
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8080, reload=False)
