2698a8ba1dce5a7fa119168ecacead663eaf7b56
OlesiaOmelchuk <olesyao2003@gmail.com>
2024-05-11 17:29:17 +0300

    created post service interface and small refactor in friendzone

---
+++ post-service/controller.py
@@ -0,0 +1,37 @@
+import fastapi
+import requests.status_codes
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, Request, Response, status
+import service
+from datetime import datetime
+
+
+app = FastAPI()
+
+id = 0
+
+
+@app.get("/get-user-posts/")
+async def get_user_posts(user: str, response: Response):
+    posts = service.get_user_posts(user)
+    response.status_code = status.HTTP_200_OK
+    return {"posts": posts}
+
+
+@app.post("/new-post/")
+async def new_post(request: Request, response: Response):
+    item = await request.json()
+    global id
+    id +=1 
+    items = {"idpost": id, "user": item["user"], "article": item["article"],  "added": datetime.now(
+    ), "modified": datetime.now(), "nlikes": 0}
+    service.new_post(items)
+    response.status_code = status.HTTP_200_OK
+
+
+@app.post("/delete-post/")
+async def delete_post(post: int, response: Response):
+    service.delete_post(post)
+    response.status_code = status.HTTP_200_OK
