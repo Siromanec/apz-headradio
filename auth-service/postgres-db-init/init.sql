eb3c77f27cc7288ef1ae44e73c6d9aa6aa7575eb
OlesiaOmelchuk <olesyao2003@gmail.com>
2024-05-11 19:04:39 +0300

    added init.sql for auth and profile service

---
+++ auth-service/postgres-db-init/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE ACCOUNT_USER_DATA
+
+CREATE TABLE ACCOUNT_USER_DATA (
+    username varchar(255),
+    email varchar(255),
+    passwordhash varchar(255)
+);
--- post-service/controller.py
+++ post-service/controller.py
@@ -25,7 +25,7 @@ async def new_post(request: Request, response: Response):
     item = await request.json()
     global id
     id +=1 
-    items = {"idpost": id, "user": item["user"], "article": item["article"],  "added": datetime.now(
+    items = {"idpost": id, "user": item["username"], "article": item["article"],  "added": datetime.now(
     ), "modified": datetime.now(), "nlikes": 0}
     service.new_post(items)
     response.status_code = status.HTTP_200_OK
