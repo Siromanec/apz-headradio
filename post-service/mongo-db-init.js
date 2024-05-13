5fb70177edd0347c8c436c9fbfd86fa78e5e65e1
Sofiia Folvarochna <sofiyafolv@gmail.com>
2024-05-13 19:49:02 +0300

    fixed post-service

--- post-service/mongo-db-init.js
+++ post-service/mongo-db-init.js
@@ -9,35 +9,31 @@ db.createUser({
     ],
 });
 
-db.createCollection("posts", 
-{
-    validator: {
-        $jsonSchema: {
-            bsonType: "object",
-            required: ["username", "post_id", "time", "text", "image"],
-            properties: {
-                username: {
-                    bsonType: "string",
-                    description: "must be a string and is required",
-                },
-                post_id: {
-                    bsonType: "int",
-                    description: "must be a int and is required",
-                },
-                time: {
-                    bsonType: "date",
-                    description: "must be a date and is required",
-                },
-                text: {
-                    bsonType: "string",
-                    description: "must be a string and is required",
-                },
-                image: {
-                    bsonType: "string",
-                    description: "must be a string and is required",
+db.createCollection("posts",
+    {
+        validator: {
+            $jsonSchema: {
+                bsonType: "object",
+                required: ["username", "post_id", "time", "article"],
+                properties: {
+                    username: {
+                        bsonType: "string",
+                        description: "must be a string and is required",
+                    },
+                    post_id: {
+                        bsonType: "int",
+                        description: "must be a int and is required",
+                    },
+                    time: {
+                        bsonType: "date",
+                        description: "must be a date and is required",
+                    },
+                    article: {
+                        bsonType: "string",
+                        description: "must be a string and is required",
+                    },
                 },
             },
         },
-    },
-});
+    });
 
--- post-service/repository.py
+++ post-service/repository.py
@@ -7,6 +7,7 @@ from bson.json_util import loads
 from bson import json_util
 import json
 import os
+import datetime
 
 client = MongoClient("mongodb://root:root_pass@localhost:27017/")
 
@@ -21,3 +22,9 @@ def new_post(items):
 
 def delete_post(post):
     collection.delete_one({"post_id": post})
+
+print(get_user_posts("user1"))
+print(new_post({"username": "user1", "post_id": 1, "time": datetime.datetime.now(), "article": "This is a post"}))
+print(get_user_posts("user1"))
+print(delete_post(1))
+print(get_user_posts("user1"))
