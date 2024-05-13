8f920695c47c819348d29a6bfc60abd7afe8d421
Sofiia Folvarochna <sofiyafolv@gmail.com>
2024-05-14 02:46:41 +0300

    added logging-service to docker

--- logging-service/main.py
+++ logging-service/main.py
@@ -1,11 +1,11 @@
 import hazelcast
 import consul
 
-consul_service = consul.Consul()
-client_name = consul_service.kv.get('cluster-name')[1]['Value'].decode()
-client = hazelcast.HazelcastClient(cluster_name=client_name)
+consul_service = consul.Consul(host="consul")
+consul_service.agent.service.register(name="logging", service_id="logging", address="logging", port=8082)
+client = hazelcast.HazelcastClient(cluster_name="dev", cluster_members=["hazelcast"])
 
-messages_queue_name = consul_service.kv.get("messages_queue_name")[1]["Value"].decode()
+messages_queue_name = "messages_queue"
 messages_queue = client.get_queue(messages_queue_name).blocking()
 
 while True:
