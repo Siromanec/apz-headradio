a3b282bb97086f1c52720beae28a6101487ff3b6
redn1njaa <ostap.seryvko@ucu.edu.ua>
2024-05-27 20:10:32 +0300

    final

--- api-gateway/routes/lifespan.py
+++ api-gateway/routes/lifespan.py
@@ -1,16 +1,19 @@
 import hazelcast
 import consul
- 
+import socket 
 from contextlib import asynccontextmanager
 
 queue = None
 
 @asynccontextmanager
 async def lifespan(app):
+    hostname = socket.gethostname()
+    ip_addr = socket.gethostbyname(hostname)
+    print(ip_addr)
     c = consul.Consul(host="gateway-consul")
     c.agent.service.register(name='api-gateway',
-                         service_id='api-gateway',
-                         address='api-gateway',
+                         service_id=f'{ip_addr}-8084',
+                         address=ip_addr,
                          port=8084)
     
     client = hazelcast.HazelcastClient(cluster_name="dev", cluster_members=["hazelcast"])
